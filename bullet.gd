402a3c9cecbeeae67cd88c8aa82216dbb26075e0
Maggen12 <129834925+Maggen12@users.noreply.github.com>
2025-08-26 16:14:19 +0200

    trail etc

--- bullet.gd
+++ bullet.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
-var speed = 2000
+var speed = 4500
+var spread_angle: float = randf_range(-2.5, 2.5)
+var spread_radians = deg_to_rad(spread_angle)
 
 func _physics_process(delta: float) -> void:
-	position += transform.x * speed * delta 
-
+	position += transform.x.rotated(spread_radians) * speed * delta
+	 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("zombies"):
 		body.take_damage(1)
